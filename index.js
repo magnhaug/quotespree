e3470424990a93a197b8d96fdb306c34f9263092
Magnus Haug <magnus@stack.no>
2014-10-05 00:01:42 +0200

    Support for text or JSON

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ var express = require('express')
 var util = require('util')
 var quotebook = require('./quotebook.json')
 
-var app = express();
+var app = express()
 
 function server (app, quotes) {
   app.set('port', (process.env.PORT || 5000))
   app.use(express.static(__dirname + '/public'))
   
+  var printers = {"json": printJson, "text": printText }
+
   app.get('/', function(request, response) {
-    response.send(getRandomQuote(quotes))
+    var bestAccepts = request.accepts(["text", "json"])
+    var printer = printers[bestAccepts];
+    var quote = getRandomQuote(quotes, printer)
+    response.send(quote)
   })
   
   app.listen(app.get('port'), function() {
@@ -17,7 +22,7 @@ function server (app, quotes) {
   })
 }
 
-function getRandomQuote(quotes) {
+function getRandomQuote(quotes, printer) {
   var categories = Object.keys(quotes)
   var category = categories[randomInt(categories.length)]
 
@@ -27,18 +32,28 @@ function getRandomQuote(quotes) {
   var quotes = quotes[category][subcategory]
   var quote = quotes[randomInt(quotes.length)]
 
+  return printer(category, subcategory, quote);
+}
+
+function printJson(category, subcategory, quote) {
+  quote.category = category
+  quote.subcategory = subcategory
+  return quote
+}
+
+function printText(category, subcategory, quote) {
   if (category === "jokes") {
-    return util.format("Joke of the day: %s", quote.joke);
+    return util.format("Joke of the day: %s", quote.joke)
   } else if (category === "quotes") {
-    return util.format("Quote of the day: \"%s\" -- %s", quote.quote, quote.author);
+    return util.format("Quote of the day: \"%s\" -- %s", quote.quote, quote.author)
   } else {
-    console.log("Unknown category", category);
+    console.log("Unknown category", category)
   }
 }
 
 function randomInt(n) {
-  return Math.floor(Math.random() * n);
+  return Math.floor(Math.random() * n)
 }
 
-server(app, quotebook);
+server(app, quotebook)
 
