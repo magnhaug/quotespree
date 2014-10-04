bf5c2a3ccc9a63b7c1ff272115d9b1df7d2d3ad2
Magnus Haug <magnus@stack.no>
2014-10-04 23:33:15 +0200

    Read jokes and quotes

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 var express = require('express')
+var fs = require('fs')
+var util = require('util')
+
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+function server (app, quotes) {
+  app.set('port', (process.env.PORT || 5000))
+  app.use(express.static(__dirname + '/public'))
+  
+  app.get('/', function(request, response) {
+    response.send(getRandomQuote(quotes))
+  })
+  
+  app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+  })
+}
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+function getRandomQuote(quotes) {
+  var categories = Object.keys(quotes)
+  var category = categories[randomInt(categories.length)]
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  var subcategories = Object.keys(quotes[category])
+  var subcategory = subcategories[randomInt(subcategories.length)]
+
+  var quotes = quotes[category][subcategory]
+  var quote = quotes[randomInt(quotes.length)]
+
+  if (category === "jokes") {
+    return util.format("Joke of the day: %s", quote.joke);
+  } else if (category === "quotes") {
+    return util.format("Quote of the day: \"%s\" -- %s", quote.quote, quote.author);
+  } else {
+    console.log("Unknown category", category);
+  }
+}
+
+function randomInt(n) {
+  return Math.floor(Math.random() * n);
+}
+
+fs.readFile('./quotebook.json', 'utf8', function (err, data) {
+  if (err) {
+    console.log('Error: ' + err);
+    return;
+  }
+  quotes = JSON.parse(data);
+  server(app, quotes);
+});
+
